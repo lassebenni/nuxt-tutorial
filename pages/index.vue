d6564f4825f35a3d8d49f1d786acb2d6bd478d79
lasseb <lbenninga@schubergphilis.com>
2021-08-23 14:58:24 +0300

    Add products pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="contaner">
     <nuxt-link to="/users">Users</nuxt-link>
+    <nuxt-link to="/products">Products</nuxt-link>
   </section>
 </template>
 
---
+++ pages/products/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="productId" />
+    <button @click="onLoadproduct">Load product</button>
+    <nuxt-link to="/products/1">Product ID 1</nuxt-link>
+    <nuxt-link to="/products/2">Product ID 2</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      productId: "",
+    };
+  },
+  methods: {
+    onLoadproduct() {
+        this.$router.push('/products/' + this.productId);
+    },
+  },
+};
+</script>
