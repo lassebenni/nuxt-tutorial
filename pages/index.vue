8023eb8ab1c99066e020cc22e30fce5c66339ea2
lasseb <lbenninga@schubergphilis.com>
2021-08-23 14:38:31 +0300

    Add users pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <section class="contaner">
+    <nuxt-link to="/users">Users</nuxt-link>
+  </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
