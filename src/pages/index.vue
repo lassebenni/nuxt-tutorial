65d9a65969c29acf56c459efa7f03ec71f81514f
Lasse Benninga <lassebenninga@godatadriven.com>
2021-10-27 11:36:39 +0200

    Add two preview articles including css

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,114 @@
 <template>
-  <section class="contaner"></section>
+  <div class="home-page">
+    <section class="intro"><h1>Latest news</h1></section>
+    <section class="featured-posts">
+      <nuxt-link :to="'/posts/' + 1" class="post-preview">
+        <article>
+          <div
+            class="post-thumbnail"
+            style="background-image: url('https://thumbs.dreamstime.com/z/new-skills-knowledge-webinar-training-business-internet-technology-concept-new-skills-knowledge-webinar-training-business-internet-121274023.jpg"
+          >
+            <div class="post-content">
+              <h1>Title</h1>
+              <p>Preview text</p>
+            </div>
+          </div>
+        </article>
+      </nuxt-link>
+      <nuxt-link :to="'/posts/' + 2" class="post-preview">
+        <article>
+          <div
+            class="post-thumbnail"
+            style="background-image: url('https://thumbs.dreamstime.com/z/new-skills-knowledge-webinar-training-business-internet-technology-concept-new-skills-knowledge-webinar-training-business-internet-121274023.jpg"
+          >
+            <div class="post-content">
+              <h1>Title</h1>
+              <p>Preview text</p>
+            </div>
+          </div>
+        </article>
+      </nuxt-link>
+    </section>
+  </div>
 </template>
 
-<script lang="ts">
-import Header from "~/components/Header.vue";
-import Vue from "vue";
-
-export default Vue.extend({
-  components: {
-    Header,
-  },
-});
+<script>
+export default {};
 </script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
--- src/pages/posts/_id/index.vue
+++ src/pages/posts/_id/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="single-post">
+  <div class="single-post-page">
     <section class="post">
       <h1>Title</h1>
       <div class="post-details">
         <div>Last updated:</div>
-        <div>Written by/div></div>
+        <div>Written by</div>
       </div>
       <p>Content:</p>
     </section>
-    <section class="comments">
+    <section class="post-feedback">
       <h2>Comments</h2>
       <div class="comment"></div>
       <div class="comment-details"></div>
@@ -18,5 +18,56 @@
 <script>
 </script>
 
-<style>
+<style scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
 </style>
