b0619c796396d419814f801903b14652a41d6fc8
Lasse Benninga <lassebenninga@godatadriven.com>
2021-10-27 16:32:01 +0200

    add admin layout

---
+++ src/layouts/admin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Zen Kaku Gothic Antique", sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
+
+<script>
+export default {
+  layout: "admin",
+};
+</script>
