4dd472edcf0664e6bdf65f3d449e6ba19ba1887c
Lasse Benninga <lassebenninga@godatadriven.com>
2021-10-27 11:04:00 +0200

    Add posts page

---
+++ src/pages/posts/_id/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="single-post">
+    <section class="post">
+      <h1>Title</h1>
+      <div class="post-details">
+        <div>Last updated:</div>
+        <div>Written by/div></div>
+      </div>
+      <p>Content:</p>
+    </section>
+    <section class="comments">
+      <h2>Comments</h2>
+      <div class="comment"></div>
+      <div class="comment-details"></div>
+    </section>
+  </div>
+</template>
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/pages/posts/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="posts-page">
+    <h1>Posts</h1>
+  </div>
+</template>
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
