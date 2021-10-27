bc21d98b2d7b978807f4b7ca23731e1d55dfe9ec
Lasse Benninga <lassebenninga@godatadriven.com>
2021-10-27 11:12:24 +0200

    Add custom font family Zen Kaku

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,11 +5,19 @@
   </div>
 </template>
 
+<style>
+html {
+  font-family: "Zen Kaku Gothic Antique", sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
+
 <script>
-import Header from '@/components/Header'
+import Header from "@/components/Header";
 
 export default {
-  components: { Header }
-}
-
+  components: { Header },
+};
 </script>
