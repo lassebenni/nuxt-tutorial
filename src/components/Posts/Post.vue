82e7fb4c5e5d886774c6ccf42034e767143aa096
Lasse Benninga <lassebenninga@godatadriven.com>
2021-10-28 23:15:07 +0200

    Add Post component

---
+++ src/components/Posts/Post.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="post">
+    <h1>{{ post.title }}</h1>
+    <div class="post-details">
+      <div>Last updated:</div>
+      <div>Written by: {{ post.author }}</div>
+    </div>
+    <p>{{ post.content }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+      validator: function (value) {
+        return value.title && value.author && value.content;
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+</style>
--- src/pages/posts/_id/index.vue
+++ src/pages/posts/_id/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="single-post-page">
-    <section class="post">
-      <h1>Title</h1>
-      <div class="post-details">
-        <div>Last updated:</div>
-        <div>Written by</div>
-      </div>
-      <p>Content:</p>
-    </section>
+    <Post :post="post" />
     <section class="post-feedback">
       <h2>Comments</h2>
       <div class="comment"></div>
@@ -15,52 +8,34 @@
     </section>
   </div>
 </template>
+
 <script>
+import Post from "@/components/Posts/Post";
+
+function MyPost(id, title, content) {
+  this.id = id;
+  this.title = title;
+  this.content = content;
+}
+
+export default {
+  components: {
+    Post,
+  },
+  data() {
+    return {
+      post: {
+        id: 1,
+        title: "Lassse",
+        content: "What's up",
+        author: "Lasse",
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
-.single-post-page {
-  padding: 30px;
-  text-align: center;
-  box-sizing: border-box;
-}
-
-.post {
-  width: 100%;
-}
-
-@media (min-width: 768px) {
-  .post {
-    width: 600px;
-    margin: auto;
-  }
-}
-
-.post-title {
-  margin: 0;
-}
-
-.post-details {
-  padding: 10px;
-  box-sizing: border-box;
-  border-bottom: 3px solid #ccc;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-
-@media (min-width: 768px) {
-  .post-details {
-    flex-direction: row;
-  }
-}
-
-.post-detail {
-  color: rgb(88, 88, 88);
-  margin: 0 10px;
-}
-
 .post-feedback a {
   color: red;
   text-decoration: none;
